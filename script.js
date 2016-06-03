2a7390877e0e1e249ba925ae3ba411d54c9ccdae
HenryHall <HenryHall@Henrys-MacBook-Pro.local>
2016-06-03 11:51:20 -0500

    small change2

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 $(document).ready(function() {
+  console.log('Document Loaded')
   $.ajax({
    type: 'GET',
     url: 'AllSets.json',
     jsonpCallback: 'jsonCallback',
     contentType: "application/json",
-    dataType: 'jsonp',
+    dataType: 'json',
     success: function(data) {
       console.log('In Success');
-       console.log(data);
+      console.log(data);
     },
     error: function(e) {
        console.log(e.message);
