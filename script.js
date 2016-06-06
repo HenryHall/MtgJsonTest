df776c6dcf132c3c6bcf3c1c94d2ab6e90622f88
HenryHall <HenryHall@Henrys-MacBook-Pro.local>
2016-06-06 16:29:31 -0500

    Update 1

--- script.js
+++ script.js
@@ -1,19 +1,73 @@
+// var http = require('http');
+//
+// http.createServer(function(req, res) {
+//   console.log("Hello World from server!");
+//   res.writeHead(200);
+//   res.write("Hello from server response...");
+//   res.end();
+// }).listen(3030);
+
+
 $(document).ready(function() {
   console.log('Document Loaded')
-  $.ajax({
-   type: 'GET',
-    url: 'AllSets.json',
-    jsonpCallback: 'jsonCallback',
-    contentType: "application/json",
-    dataType: 'json',
-    success: function(data) {
-      console.log('In Success');
-      console.log(data);
-    },
-    error: function(e) {
-       console.log(e.message);
+
+
+  $('#searchSets').click(function(){
+    var searchQuery = this.innerHTML;
+    var cardLibrary;
+    console.log(searchQuery);
+
+    $.ajax({
+     type: 'GET',
+      url: 'JSon/AllSets.json',
+      jsonpCallback: 'jsonCallback',
+      contentType: "application/json",
+      dataType: 'json',
+      success: function(data) {
+        console.log('In Success');
+        console.log(data);
+        cardLibrary = data;
+      },
+      error: function(e) {
+         console.log(e.message);
+      }
+    });
+
+    for (var i in cardLibrary) {
+      if (cardLibrary[i].name = searchQuery) {
+        console.log(cardLibrary[i]);
+      }
     }
+
   });
 
+  $('#searchCards').click(function(){
+    var searchQuery = this.innerHTML;
+    var cardLibrary;
+    console.log(searchQuery);
+
+    $.ajax({
+     type: 'GET',
+      url: 'JSon/AllCards.json',
+      jsonpCallback: 'jsonCallback',
+      contentType: "application/json",
+      dataType: 'json',
+      success: function(data) {
+        console.log('In Success');
+        console.log(data);
+        cardLibrary = data;
+      },
+      error: function(e) {
+         console.log(e.message);
+      }
+    });
+
+    for (var i in cardLibrary) {
+      if (cardLibrary[i].name = searchQuery) {
+        console.log(cardLibrary[i]);
+      }
+    }
+
+  });
 
 });
